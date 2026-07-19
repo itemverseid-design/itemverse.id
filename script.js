f7e81345015b32e1665363ddae51deebffe70270
itemverseid-design <itemverse.id@gmail.com>
2026-07-19 14:56:01 +0700

    update website itemverse
    update website itemverse

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+function kirimOrder() {
+    var nama = document.getElementById("nama").value;
+    var layanan = document.getElementById("layanan").options[document.getElementById("layanan").selectedIndex].text;
+    var target = document.getElementById("target").value;
+    var jumlah = document.getElementById("jumlah").value;
+
+    var pesan = "Halo ITEMVERSE, saya ingin order.\n\n" +
+        "👤 Nama: " + nama + "\n" +
+        "📦 Layanan: " + layanan + "\n" +
+        "🔗 Target: " + target + "\n" +
+        "📊 Jumlah: " + jumlah;
+
+    window.open("https://wa.me/6287785080652?text=" + encodeURIComponent(pesan), "_blank");
+}
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+  topBtn.style.display = window.scrollY > 300 ? "block" : "none";
+});
+
+topBtn.onclick = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
